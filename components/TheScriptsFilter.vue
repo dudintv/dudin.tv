e798ecfb9d1bc36ad3fab5ff4687b019cf6de8fe
Dmitry Dudin <svetoz@gmail.com>
2020-03-15 02:52:01 +0100

    Create TheScriptFilter component

---
+++ components/TheScriptsFilter.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  #scripts-filter.flex.items-start.cursor-pointer.text-white.font-bold
+    .option.px-3.py-1.hover_border-b-4.border-white(
+      v-for="option in options"
+      :style="gradByName(option)"
+      ) {{ option }}
+</template>
+
+<script>
+export default {
+  data: () => ({
+    options: [
+      'all',
+      'animation',
+      'link',
+      'logic',
+      'position',
+      'texture',
+    ]
+  }),
+  methods: {
+    gradByName (name) {
+      const gradient = this.$store.state.gradients[name]
+      return `background: linear-gradient(to bottom right, ${gradient[0]}, ${gradient[1]})`
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,30 @@
   .content
     TheIntro
     TheScriptsLogo
+    .flex.justify-around
+      TheScriptsFilter
+      .flex.items-start
+        img(src="~/static/images/common/copy.svg")
+        span.mx-2 —
+        span.leading-tight
+          | one-click copying code.
+          br
+          i Be sure, you always
+          br
+          i get the latest version!
     section
 </template>
 
 <script>
 import TheIntro from '~/components/TheIntro'
 import TheScriptsLogo from '~/components/TheScriptsLogo'
+import TheScriptsFilter from '~/components/TheScriptsFilter'
 
 export default {
   components: {
     TheIntro,
     TheScriptsLogo,
+    TheScriptsFilter,
   }
 }
 </script>
