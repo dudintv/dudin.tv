e0e2f42895a7715f6dab81239b8e6ab64dcc1423
Dmitry Dudin <svetoz@gmail.com>
2020-03-15 02:51:16 +0100

    Create gradient colors in the Store

---
+++ store/index.js
@@ -0,0 +1,10 @@
+export const state = () => ({
+  gradients: {
+    all: ['#7940CC', '#234CA9'],
+    animation: ['#F2858A', '#EFC0A0'],
+    link: ['#79D6FF', '#61A3FF'],
+    logic: ['#C45ADD', '#7F4EF4'],
+    position: ['#B6E78C', '#2DCFE6'],
+    texture: ['#C389F0', '#ED7485'],
+  }
+})
