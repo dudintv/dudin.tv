a0b70607d9c396dd94611e84d6297d3a44c44f1b
Dmitry Dudin <svetoz@gmail.com>
2020-03-16 23:14:50 +0100

    Add shadows for ScriptsLogo

--- components/TheScriptsLogo.vue
+++ components/TheScriptsLogo.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
-  #scripts-logo(ref="logo")
+  .scripts-logo-container.relative
+    .back-space
+    #scripts-logo
+    img.ellipse-shadow(src="~/static/images/common/ellipse-shadow.svg")
 </template>
 
 <script>
@@ -19,5 +22,13 @@ export default {
 </script>
 
 <style>
-
+  .back-space {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: linear-gradient(179.4deg, rgba(73, 35, 54, 0) 1.48%, rgba(39, 6, 22, 0.77) 53.55%, rgba(48, 14, 30, 0.67) 67.2%, rgba(64, 30, 46, 0.478571) 74.27%, rgba(61, 29, 44, 0.287143) 80.84%, rgba(63, 29, 45, 0) 98.54%);
+    mix-blend-mode: luminosity;
+  }
 </style>
