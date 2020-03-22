38a759a7c7babb4a9c962778310ebe63f9ca0444
Dmitry Dudin <svetoz@gmail.com>
2020-03-22 04:05:00 +0100

    Create MediaImage component

---
+++ components/media/MediaImage.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  img(:src="src")
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: '',
+    }
+  },
+  computed: {
+    src () {
+      return `/images${this.$route.path}/${this.name}`
+    }
+  },
+  mounted () {
+    console.log('MediaImage created')
+  }
+}
+</script>
