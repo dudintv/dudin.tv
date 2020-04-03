b7df3c33680dbd9932be16a93f18c13857faeeb9
Dmitry Dudin <svetoz@gmail.com>
2020-04-03 04:05:38 +0200

    Add debounce effect to changing of youtube width

--- components/media/MediaYoutube.vue
+++ components/media/MediaYoutube.vue
@@ -1,11 +1,27 @@
 <template lang="pug">
   .media-youtube
     .youtube-wrapper
-      .youtube-player(ref="player")
+      .youtube-player(ref="player" :id="videoId")
 </template>
 
 <script>
 import YouTubePlayer from 'youtube-player'
+
+const debounce = (action, time) => {
+  let timer
+  return (...args) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => action(...args), time)
+  }
+}
+
+const youtubeWidthChanged = debounce((vThis) => {
+  const youtubePlayer = document.getElementById(vThis.videoId)
+  if (youtubePlayer) {
+    youtubePlayer.setAttribute('width', vThis.videoWidth)
+    youtubePlayer.setAttribute('height', vThis.videoHeight)
+  }
+}, 500)
 
 export default {
   props: {
@@ -18,31 +34,55 @@ export default {
       default: 0
     }
   },
+  computed: {
+    videoId () {
+      let id
+      if (/v=.+$/.test(this.url)) {
+        id = this.url.match(/v=(.+)$/)[1]
+      } else {
+        id = this.url.match(/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/)[1]
+      }
+      return id
+    },
+    videoWidth () {
+      if (this.width > 0) {
+        return this.width
+      } else {
+        return (this.$el).offsetWidth
+      }
+    },
+    videoHeight () {
+      if (this.width > 0) {
+        return this.width * 9 / 16
+      } else {
+        return (this.$el).offsetHeight
+      }
+    },
+  },
+  watch: {
+    width () {
+      this.changeYoutubeSize()
+    }
+  },
   mounted () {
-    let videoId
-    if (/v=.+$/.test(this.url)) {
-      videoId = this.url.match(/v=(.+)$/)[1]
-    } else {
-      videoId = this.url.match(/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/)[1]
-    }
-    let videoWidth, videoHeight
-    if (this.width > 0) {
-      videoWidth = this.width
-      videoHeight = this.width * 9 / 16
-    } else {
-      videoWidth = (this.$el).offsetWidth
-      videoHeight = (this.$el).offsetHeight
-    }
-    if (videoId) {
-      YouTubePlayer(
-        this.$refs.player,
-        {
-          videoId,
-          width: videoWidth,
-          height: videoHeight,
-        }
-      )
-    }
+    this.loadYoutube()
+  },
+  methods: {
+    loadYoutube () {
+      if (this.videoId) {
+        YouTubePlayer(
+          this.$refs.player,
+          {
+            videoId: this.videoId,
+            width: this.videoWidth,
+            height: this.videoHeight,
+          }
+        )
+      }
+    },
+    changeYoutubeSize () {
+      youtubeWidthChanged(this)
+    },
   },
 }
 </script>
