84fc0a58bdeb305b13dd8edeab999240fc1f73ea
Dmitry Dudin <svetoz@gmail.com>
2020-04-05 22:52:02 +0200

    Create Links page

---
+++ pages/links.vue
@@ -0,0 +1,61 @@
+<template lang="pug">
+  .links-page.conatiner.mx-auto
+    #links-bg
+    .content
+      h1 Vizrt lessons
+      p
+        | Official, by Vizrt —&nbsp;
+        a(href="http://vizuniversity.learnupon.com" target="_blank") vizuniversity.learnupon.com
+        br
+        | tutsGFX —&nbsp;
+        a(href="http://tutsgfx.com/free-vizrt-training" target="_blank") tutsgfx.com/free-vizrt-training
+      h1.mt-8 Vizrt scripts
+      p
+        a(href="http://erizos.tv/scripts" target="_blank") erizos.tv/scripts
+        br
+        a(href="http://viztoolkit.com" target="_blank") viztoolkit.com
+      h1.mt-8 Vizrt tools
+      p
+        a(href="https://idonix.com/graphics/viz-script-notepadplusplus?fbclid=IwAR0HiX_9Z-TkM5fAKYg4tqpVzUarByCZqlLeJ7ysaNpTfpCbfTfuIVwEsSw" target="_blank") IDONIX Notepad++ conector for scripting
+        br
+        | VBN Converter —
+        a(href="https://app.box.com/s/i8e6djm6moewtxfu550c848052bouri2" target="_blank") Viz Image Converter 3.0.0.14629
+      small
+        | If some link doesn’t work, please let me know facebook.com/dudintv
+</template>
+
+<script>
+import lottie from 'lottie-web'
+
+export default {
+  mounted () {
+    lottie.loadAnimation({
+      container: document.getElementById('links-bg'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: '/animations/bg-rock.json',
+      speed: 500,
+    }).setSpeed(3)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .links-page {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    margin-top: -10%;
+    margin-bottom: -10%;
+  }
+
+  .content {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    z-index: 100;
+  }
+</style>
