9bd391b2c659c37bc564f46187ce11c9bc63d10e
Dmitry Dudin <dudintv@gmail.com>
2020-04-19 23:59:25 +0200

    Change config to generate dynamic pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,83 +13,90 @@ async function getDynamicPaths (urlFilepathTable) {
   )
 }
 
-export default {
-  mode: 'universal',
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: 'dudin.tv',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Vizrt scripts and article from Dmitry Dudin' },
-      { name: 'msapplication-TileColor', content: '#603cba' },
-      { name: 'theme-color', content: '#ffffff' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' },
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' },
-      { rel: 'manifest', href: '/favicons/site.webmanifest' },
-      { rel: 'mask-icon', href: '/favicons/safari-pinned-tab.svg', color: '#9804d0' }
-    ]
-  },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#243B61' },
-  /*
-  ** Global CSS
-  */
-  css: [
-    '~assets/css/fonts',
-    '~assets/css/main',
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '~plugins/vue-code-highlight.js'
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss'
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
-  /*
-  ** Build configuration
-  */
-  build: {
+export default async () => {
+  return {
+    mode: 'universal',
     /*
-    ** You can extend webpack config here
+    ** Headers of the page
     */
-    extend (config, ctx) {
-    // add frontmatter-markdown-loader
-      config.module.rules.push({
-        test: /\.md$/,
-        include: path.resolve(__dirname, 'content'),
-        loader: 'frontmatter-markdown-loader',
-        options: {
-          mode: [Mode.VUE_COMPONENT, Mode.META]
-        }
+    head: {
+      title: 'dudin.tv',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Vizrt scripts and article from Dmitry Dudin' },
+        { name: 'msapplication-TileColor', content: '#603cba' },
+        { name: 'theme-color', content: '#ffffff' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/favicons/site.webmanifest' },
+        { rel: 'mask-icon', href: '/favicons/safari-pinned-tab.svg', color: '#9804d0' }
+      ]
+    },
+    /*
+    ** Customize the progress-bar color
+    */
+    loading: { color: '#243B61' },
+    /*
+    ** Global CSS
+    */
+    css: [
+      '~assets/css/fonts',
+      '~assets/css/main',
+    ],
+    /*
+    ** Plugins to load before mounting the App
+    */
+    plugins: [
+      '~plugins/vue-code-highlight.js'
+    ],
+    /*
+    ** Nuxt.js dev-modules
+    */
+    buildModules: [
+      // Doc: https://github.com/nuxt-community/eslint-module
+      '@nuxtjs/eslint-module',
+      // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+      '@nuxtjs/tailwindcss'
+    ],
+    /*
+    ** Nuxt.js modules
+    */
+    modules: [
+    ],
+    /*
+    ** Build configuration
+    */
+    build: {
+      /*
+      ** You can extend webpack config here
+      */
+      extend (config, ctx) {
+      // add frontmatter-markdown-loader
+        config.module.rules.push({
+          test: /\.md$/,
+          include: path.resolve(__dirname, 'content'),
+          loader: 'frontmatter-markdown-loader',
+          options: {
+            mode: [Mode.VUE_COMPONENT, Mode.META]
+          }
+        })
+      }
+    },
+
+    generate: {
+      routes: await getDynamicPaths({
+        '/scripts': 'scripts/*.md',
+        '/articles': 'articles/*.md',
       })
+    },
+
+    purgeCSS: {
+      whitelistPatterns: [/^page/],
     }
-  },
-
-  generate: {},
-
-  purgeCSS: {
-    whitelistPatterns: [/^page/],
   }
 }
