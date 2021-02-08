aa5085e0bf9b22cd26df3fd82a56e22697231b85
Dmitry Dudin <svetoz@gmail.com>
2021-02-08 23:39:34 +0100

    add new script: Temo

--- components/scripts/ScriptItem.vue
+++ components/scripts/ScriptItem.vue
@@ -5,6 +5,11 @@
       :style="gradByName(script.attributes.category)"
       )
       img(:src="thumbnailUrl()")
+      .viz4-label(v-if="script.attributes.viz4")
+        <svg width="21" height="30" viewBox="0 0 21 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 14.7531V26.7284L1.46809 23.3333L18.1277 11.9136V0L16.5957 3.39506L0 14.7531Z" fill="white"/>
+          <path d="M9.25532 24.4444H4.78723L18.1277 15.1235V17.0988H21V24.4444H18.1277V30H9.25532V24.4444Z" fill="white"/>
+        </svg>
       .hover
     .header.relative
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 340 82" class="header-bg">
@@ -83,89 +88,97 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .script-item {
-    background-color: #455C82;
-    max-width: 355px;
-    box-shadow: 0 0px 0px rgba(0,0,0,0.4);
-    transition: box-shadow 0.2s ease-in-out;
-    z-index: 10;
+.script-item {
+  background-color: #455C82;
+  max-width: 355px;
+  box-shadow: 0 0px 0px rgba(0,0,0,0.4);
+  transition: box-shadow 0.2s ease-in-out;
+  z-index: 10;
+  transition: background-color .1s linear;
+
+  &:hover {
+    box-shadow: 0 7px 30px rgba(0,0,0,0.4);
+    z-index: 100;
+    background-color: rgb(52, 81, 128);
+  }
+}
+
+.thumbnail {
+  position: relative;
+  width: 355px;
+  height: 200px;
+
+  .hover {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 50;
+    background-color: hsla(0, 100%, 100%, 0);
     transition: background-color .1s linear;
-
-    &:hover {
-      box-shadow: 0 7px 30px rgba(0,0,0,0.4);
-      z-index: 100;
-      background-color: rgb(52, 81, 128);
-    }
   }
 
-  .thumbnail {
-    position: relative;
-    width: 355px;
-    height: 200px;
-
+  &:hover {
     .hover {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      z-index: 50;
-      background-color: hsla(0, 100%, 100%, 0);
-      transition: background-color .1s linear;
-    }
-
-    &:hover {
-      .hover {
-        background-color: hsla(0, 100%, 100%, 0.2);
-      }
+      background-color: hsla(0, 100%, 100%, 0.2);
     }
   }
+}
 
-  .category {
-    font-size: .8rem;
-    font-weight: bold;
+.category {
+  font-size: .8rem;
+  font-weight: bold;
+}
+
+.title {
+  line-height: 0.8em;
+  color: white;
+}
+
+.code {
+  cursor: pointer;
+  color: white;
+  font-weight: bold;
+  font-size: 14px;
+  display: inline-flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  padding-right: 5px;
+  margin-left: 10px;
+  width: 24px;
+  height: 28px;
+  background: url(/images/common/copy.svg);
+  opacity: 0.5;
+  &:hover {
+    opacity: 1;
   }
+}
 
-  .title {
-    line-height: 0.8em;
-    color: white;
+.no-code {
+  display: block;
+  border: 3px solid rgba(255,255,255,0.2);
+  border-radius: 4px;
+  padding: 0rem 0.3rem 0.2rem 0.3rem;
+  line-height: 0.8rem;
+  font-size: 0.8rem;
+  color: white;
+
+  &:hover {
+    border-color: white;
   }
+}
 
-  .code {
-    cursor: pointer;
-    color: white;
-    font-weight: bold;
-    font-size: 14px;
-    display: inline-flex;
-    justify-content: flex-end;
-    align-items: flex-end;
-    padding-right: 5px;
-    margin-left: 10px;
-    width: 24px;
-    height: 28px;
-    background: url(/images/common/copy.svg);
-    opacity: 0.5;
-    &:hover {
-      opacity: 1;
-    }
-  }
+.description {
+  padding: 5px 30px 10px;
+  line-height: 1.4em;
+}
 
-  .no-code {
-    display: block;
-    border: 3px solid rgba(255,255,255,0.2);
-    border-radius: 4px;
-    padding: 0rem 0.3rem 0.2rem 0.3rem;
-    line-height: 0.8rem;
-    font-size: 0.8rem;
-    color: white;
-
-    &:hover {
-      border-color: white;
-    }
-  }
-
-  .description {
-    padding: 5px 30px 10px;
-    line-height: 1.4em;
-  }
+.viz4-label {
+  position: absolute;
+  bottom: 1rem;
+  right: 1.5rem;
+  opacity: 0.6;
+  mix-blend-mode: screen;
+}
 </style>
---
+++ content/scripts/temo-sprites.md
@@ -0,0 +1,63 @@
+---
+title: Sprites (Temo for Viz4)
+description: Analogue of original Vizrt Temo plugin, only for Viz4 fusion material. Work as a traditional sprites pleer.
+category: texture
+link: https://bitbucket.org/vizrt/vizartist-script/src/master/dudin-texture/dudin-temo-sprite/
+file: dudin-temo-sprite.vb
+viz4: true
+---
+
+<interface-description image="viz4-index.png">
+
+### Interface description
+
+__Viz version__ allows you to choose what is changed by this script. __Viz 3__ change legacy texture plugin. __Viz 4__ change texture params into fusion material.
+
+__Numbers of tiles (vertical and horizontal)__ cuts the image as much as it has frames accordingly.
+
+__Selection mode__ gives classic __number__ mode where you change the index of the frame, __name__ where you can name each cell and show by name, without index.
+
+__Main axis__ works only with __number__ mode. It changes the direction of the calculation order. "X" is the classic one.
+
+__Show tile number__ works only with __number__ mode. It is a cell selector. 
+
+__Crop X / Crop Y__ define how much we cut each cell in the percentage of the size.
+
+__Lock crop__ allows you conveniently change crop X and Y by one number.
+
+</interface-description>
+
+I've created this script as an analogue of original Vizrt build-in plugin "Temo". But only for Viz4 fusion material. It implements the classic "sprite" technique for graphics engines.
+
+Disclaimer: 
+This script changes texture scaling and position via "SendCommand" function. I know that it isn't a good way, but for the current VizArtist 4.2 version, it's a single option to do this, unfortunately. If Vizrt developers will allow changing these parameters via normal script object — I'll upgrade my script this accordingly.
+
+I did not repeat only the standard behaviour of Temo. Also, I implement a few my improving ideas:
+
+1. Now, you can change "main axis". For some sprite images it's more convenient to count cells from top to bottom, and only after that count from left to right. <br><br>
+Example, where the main axis is "Y":
+<media-image name="names_example_2x8.png" />
+
+2. I added the ability to name every single cell. So, you can use scripts much more flexible way. Forget about indexes, now you can use names! <br><br>
+For example, you can add a new cell (change image) in your pictures without problem with rebuilding indexes — just add a new row and name them. You can send an obvious name an instead strange number from Pilot/Trio template. For another example — create sprite images with social networks and set it by name. And now, you can send "facebook" instead strange "2" (or "1", or "5", ...). <br><br>
+This "name" approach allows you: a) reduce your scene tree, use only one container instead Omo plugin with a many sub-containers, and it's easier to handle one single material; b) manage your icons set more convenient way, all frames will be on one screen; c) have a nice debug helper for Pilot template and for VizEngine console.
+<media-image name="smiles_sprites_4x2.png" />
+<media-image name="name-mode.png" />
+
+3. I added cropping ;) Now, you can leave a grid on the image, and just crop it in your scene. Also, you can add a title for each "frame" for convenient using. Take a look at my example with weather icons. <br><br>
+For example:
+<media-image name="weather_4x3_with_titles.png" />
+
+After adding these improvements I decided to make "Viz 3" mode :)
+
+<interface-description image="viz3-name.png">
+
+### "Name" mode
+
+Here you find two different fields.
+
+__Names__ allow you to give names to all cells. Rows are separated by "new line". Columns are separated by "|" char.
+
+__Show name__ is a cell selector. Like "Show tile number" but only by a string.
+
+</interface-description>
