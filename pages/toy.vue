8d04cf3b91b20834e5115db562a30c1993161dcb
Dmitry Dudin <ddudin@harlemnext.com>
2022-07-13 23:45:45 +0200

    feat: make a preview developing page for toys

---
+++ pages/toy.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  .container.mx-auto.h-full
+    Toy
+</template>
+
+<script>
+import Toy from '~/components/toys/MultiFollow'
+
+export default {
+  components: {
+    Toy,
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
